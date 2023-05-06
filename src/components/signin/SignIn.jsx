bdc2037e5341b6d632e95cd57c1dcc5e6f5df5c7
nicobonder <nicobonder@hotmail.com>
2023-05-06 15:54:05 -0300

    errors works ok

--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 //Importo UserAuth para tener las funciones de AuthContext
 import { UserAuth } from "@/context/AuthContext";
 import { useRouter } from "next/router";
@@ -7,10 +7,27 @@ import Swal from "sweetalert2";
 import logo from "../../../assets/NEXT.png";
 import Image from "next/image";
 
+//FUNCION VALIDADORA
+function validate(input) {
+  //va a recibir el estado input con los cambios detectados por los handlers
+  let errors = {}; //objeto que guarda todos los errores y le agrego props con los nombres iguales a los del input
+  if (!input.email) {
+    errors.email = "You must enter your mail";
+  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(input.email)) {
+    errors.email = "It must be a valid email";
+  } else if (!input.password) {
+    errors.password = "You need you enter your password";
+  } else if (!/^(?=.*\d).{6,8}$$/.test(input.password)) {
+    errors.password =
+      "Remember: It has between 6 and 8 characters. And includs a number.";
+  }
+  return errors; //se retorna el obj errors con la prop y el string correspondiente. ej: let errors ={name: 'a name is required'}
+}
+
 export default function SignIn() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [error, setError] = React.useState("");
+  const [errors, setErrors] = React.useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   const router = useRouter();
 
@@ -47,27 +64,36 @@ export default function SignIn() {
   //   : router.push("/");
   // }, []);
 
+  const handleChange = () => {
+    setErrors(
+      validate({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      })
+    );
+  };
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError("");
+    setErrors("");
     try{
-        await signIn(email, password);
+        await signIn(emailRef.current.value, passwordRef.current.value);
         router.push("/");
     } catch (error) {
         switch (error.code) {
           case "auth/user-not-found":
-            setError(
+            setErrors(
               "User not found.Please, check your email."
               );
               userNotFound()
             break;
           case "auth/wrong-password":
-            setError("Password is incorrect.");
+            setErrors("Password is incorrect.");
             wrongPassword()
             break;
           default:
-            setError(error.message);
+            setErrors(error.message);
             errorDefault()
         }
       }
@@ -78,22 +104,34 @@ export default function SignIn() {
       <form className="signup_form" onSubmit={handleSubmit}>
         <div className="input_wrap">
           <label className="label_form">Email adress:</label>
+        <div className="labelInput">
           <input
-            onChange={(e) => setEmail(e.target.value)}
             className="input_form"
             type="email"
+            id="email"
+            name="email"
+            ref={emailRef}
             placeholder="Enter your email"
+            onChange={handleChange}
           />
+          {errors.email && <p className="signup_error">{errors.email}</p>}
+          </div>
         </div>
 
         <div className="input_wrap">
           <label className="label_form">Password:</label>
+        <div className="labelInput">
           <input
-            onChange={(e) => setPassword(e.target.value)}
             className="input_form"
             type="password"
+            id="password"
+            name="password"
+            ref={passwordRef}
             placeholder="Enter your Password"
+            onChange={handleChange}
           />
+          {errors.password && <p className="signup_error">{errors.password}</p>}
+          </div>
         </div>
 
         <button className="sign_btn">Sign In</button>
@@ -102,3 +140,143 @@ export default function SignIn() {
     </div>
   );
 }
+
+/*import React, { useEffect, useState, useRef } from "react";
+//Importo UserAuth para tener las funciones de AuthContext
+import { UserAuth } from "@/context/AuthContext";
+import { useRouter } from "next/router";
+
+import Swal from "sweetalert2";
+import logo from "../../../assets/NEXT.png";
+import Image from "next/image";
+
+
+//FUNCION VALIDADORA
+function validate(input) {
+  //va a recibir el estado input con los cambios detectados por los handlers
+  let errors = {}; //objeto que guarda todos los errores y le agrego props con los nombres iguales a los del input
+  if (!input.email) {
+    errors.email = "You must enter an mail";
+  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(input.email)) {
+    errors.email = "It must be a valid email";
+  } else if (!input.password) {
+    errors.password = "You need you enter a password";
+  } else if (!/^(?=.*\d).{6,8}$$/.test(input.password)) {
+    errors.password =
+      "It should have between 6 and 8 characters. And must includ a number.";
+  }
+  return errors; //se retorna el obj errors con la prop y el string correspondiente. ej: let errors ={name: 'a name is required'}
+}
+
+export default function SignIn() {
+  const [errors, setErrors] = React.useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const router = useRouter();
+
+  const { signIn, isUserAuthenticated } = UserAuth();
+
+  const userNotFound = () => {
+    Swal.fire({
+      title: "NEXT EVENT",
+      html: "User not found. Please, check your email.",
+      footer: "<p>Please, try again.</p>",
+    });
+  };
+
+  const wrongPassword = () => {
+    Swal.fire({
+      title: "NEXT EVENT",
+      html: "Password is incorrect.",
+      footer: "<p>Please, try again.</p>",
+    });
+  };
+
+  const errorDefault = () => {
+    Swal.fire({
+      title: "NEXT EVENT",
+      html: "There was some mistake.",
+      footer: "<p>Please, try again.</p>",
+    });
+  };
+
+  // useEffect(() => {
+  //   // checks if the user is authenticated
+  //   !!isUserAuthenticated()
+  //   ? router.push("/signin")
+  //   : router.push("/");
+  // }, []);
+
+  const handleChange = () => {
+    setErrors(
+      validate({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      })
+    );
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrors("");
+    try {
+      await signIn(email, password);
+      router.push("/");
+    } catch (error) {
+      switch (error.code) {
+        case "auth/user-not-found":
+          setErrors("User not found. Please, check your email.");
+          userNotFound();
+          break;
+        case "auth/wrong-password":
+          setErrors("Password is incorrect.");
+          wrongPassword();
+          break;
+        default:
+          setErrors(error.message);
+          errorDefault();
+      }
+    }
+  };
+
+  return (
+    <div className="signIn_wrap">
+      <form className="signup_form" onSubmit={handleSubmit}>
+        <div className="input_wrap">
+          <div className="labelInput">
+            <label className="label_form">Email adress:</label>
+            <input
+              className="input_form"
+              type="email"
+              name="email"
+              ref={emailRef}
+              placeholder="Enter your email"
+              onChange={handleChange}
+            />
+          </div>
+          {errors.email && <p className="signup_error">{errors.email}</p>}
+        </div>
+
+        <div className="input_wrap">
+          <div className="labelInput">
+            <label className="label_form">Password:</label>
+            <input
+              className="input_form"
+              type="password"
+              name="password"
+              ref={passwordRef}
+              placeholder="Enter your Password"
+              onChange={handleChange}
+            />
+          </div>
+          {errors.password && <p className="signup_error">{errors.password}</p>}
+        </div>
+
+        <button className="sign_btn">Sign In</button>
+      </form>
+      <div className="separator"></div>
+    </div>
+  );
+}
+*/
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { UserAuth } from "../../../context/AuthContext";
 import { useRouter } from "next/router";
 
@@ -6,109 +6,133 @@ import Swal from "sweetalert2";
 import logo from "../../../assets/NEXT.png";
 import Image from "next/image";
 
+//FUNCION VALIDADORA
+function validate(input) {
+  //va a recibir el estado input con los cambios detectados por los handlers
+  let errors = {}; //objeto que guarda todos los errores y le agrego props con los nombres iguales a los del input
+  if (!input.email) {
+    errors.email = "You must enter an mail";
+  } else if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(input.email)) {
+    errors.email = "It must be a valid email";
+  } else if (!input.password) {
+    errors.password = "You need you enter a password";
+  } else if (!/^(?=.*\d).{6,8}$$/.test(input.password)) {
+    errors.password =
+      "It should have between 6 and 8 characters. And must includ a number.";
+  }
+  return errors; //se retorna el obj errors con la prop y el string correspondiente. ej: let errors ={name: 'a name is required'}
+}
+
 export default function Signup() {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [error, setError] = React.useState("");
-
   const router = useRouter();
-
   const { createUser } = UserAuth();
+  const [errors, setErrors] = React.useState("");
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
   const registeredMessage = () => {
-    const imgSrc = logo.src;
-    const imgWidth = logo.width;
-    const imgHeight = logo.height;
-
-    const img = <Image src={imgSrc} width={imgWidth} height={imgHeight} />;
+    // const imgSrc = logo.src;
+    // const imgWidth = logo.width;
+    // const imgHeight = logo.height;
+    // const img = <Image src={imgSrc} width={imgWidth} height={imgHeight} />;
 
     Swal.fire({
-        title: "NEXT EVENT",
-        html:"Now you will receive updates with all our events.",
-        footer: "<p>Thanks for registering.</p>",
+      title: "NEXT EVENT",
+      html: "Now you will receive updates with all our events.",
+      footer: "<p>Thanks for registering.</p>",
     });
   };
 
   const errorRegisting = () => {
-    const imgSrc = logo.src;
-    const imgWidth = logo.width;
-    const imgHeight = logo.height;
-
-    const img = <Image src={imgSrc} width={imgWidth} height={imgHeight} />;
-
     Swal.fire({
-        title: "NEXT EVENT",
-        html:"Email is already in use. ",
-        footer: "<p>You need a different email.</p>",
+      title: "NEXT EVENT",
+      html: "Email is already in use.",
+      footer: "<p>You need a different email.</p>",
     });
   };
 
   const errorDefault = () => {
-    const imgSrc = logo.src;
-    const imgWidth = logo.width;
-    const imgHeight = logo.height;
-
-    const img = <Image src={imgSrc} width={imgWidth} height={imgHeight} />;
-
     Swal.fire({
-        title: "NEXT EVENT",
-        html:"There was an error registering.",
-        footer: "<p>Please, try again.</p>",
+      title: "NEXT EVENT",
+      html: "There was an error registering.",
+      footer: "<p>Please, try again.</p>",
     });
   };
 
-  const goToAccount = () => {
-    router.push("/account");
-  }
+  // const goToAccount = () => {
+  //   router.push("/account");
+  // };
+
+  const handleChange = () => {
+    setErrors(
+      validate({
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      })
+    );
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError("");
+    setErrors("");
     try {
-      await createUser(email, password);
+      await createUser(emailRef.current.value, passwordRef.current.value);
       registeredMessage();
       router.push("/");
     } catch (errors) {
-        switch (errors.code) {
-          case "auth/email-already-in-use":
-            setError("Email is already in use. You need a different email.");
-            errorRegisting()
-            break;
-          default:
-            setError(errors.message);
-            errorDefault();
-        }
-        console.log(errors.message)
+      switch (errors.code) {
+        case "auth/email-already-in-use":
+          setErrors("Email is already in use. You need a different email.");
+          errorRegisting();
+          break;
+        default:
+          setErrors(errors.message);
+          errorDefault();
       }
+      console.log(errors.message);
+    }
   };
 
   return (
     <div className="signup_page">
-
       <form className="signup_form" onSubmit={handleSubmit}>
         <div className="input_wrap">
-          <label className="label_form">Email adress</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            className="input_form"
-            type="email"
-            placeholder="Enter your email"
-          />
+            <label className="label_form">Email adress:</label>
+          <div className="labelInput">
+            <input
+              className="input_form"
+              type="email"
+              id="email"
+              name="email"
+              ref={emailRef}
+              placeholder="Enter your email"
+              onChange={handleChange}
+            />
+          {errors.email && <p className="signup_error">{errors.email}</p>}
+          </div>
         </div>
 
         <div className="input_wrap">
-          <label className="label_form">Password:</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            className="input_form"
-            type="password"
-            minLength="6"
-            placeholder="Enter your Password"
-          />
+            <label className="label_form">Password:</label>
+          <div className="labelInput">
+            <input
+              className="input_form"
+              type="password"
+              id="password"
+              name="password"
+              ref={passwordRef}
+              placeholder="Enter your Password"
+              onChange={handleChange}
+            />
+          {errors.password && <p className="signup_error">{errors.password}</p>}
+          </div>
         </div>
 
         <button className="signup_btn">Register!</button>
-        <p>If you register, you will receive our newsletter with all the information about new events!</p>
+        <p>
+          If you register, you will receive our newsletter with all the
+          information about new events!
+        </p>
       </form>
     </div>
   );
